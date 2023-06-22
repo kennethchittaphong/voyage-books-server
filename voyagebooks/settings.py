8f5ed777eacb92dbf3593df06594e4f2c8444379
Kenneth Chittaphong <kennethchittaphong@gmail.com>
2023-06-21 20:26:55 -0500

    fixed

--- voyagebooks/settings.py
+++ voyagebooks/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 import django_on_heroku
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,7 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://voyage-books.netlify.app/'
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
